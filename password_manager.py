0c9f8c222a32e3abb1e58a0d5c504dd822e2176d
davidcxpardede <davidpardede40@gmail.com>
2023-05-04 16:34:24 +0700

    Commit

---
+++ password_manager.py
@@ -0,0 +1,10 @@
+pwd = input("What is the master password? ")
+
+mode = input("Would you like to add a new password or view existing ones (type q to quit)? ").lower()
+
+while True:
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    print("Invalid mode.")
