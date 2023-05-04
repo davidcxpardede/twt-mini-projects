376c19ec2bb32b8987e454761412d4a4cf0155ff
davidcxpardede <davidpardede40@gmail.com>
2023-05-04 16:18:05 +0700

    Adventure

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,20 @@
+name = input("Type your name: ")
+print("Welcome", name, "to the grandest adventure of your whole life!")
+
+print("You are currently a chemistry teacher.")
+print("You have a lovely wife and son.")
+print("Your wife's sister is kind and his DEA husband is funny.")
+print("But you are struggling in making money.")
+
+answer = input("What other career should you pursue now?").lower()
+
+if answer == "keep working as a teacher":
+    print("You are a pathetic human being!")
+elif answer == "get a secondary job":
+    print("Do your stupid little brain think that will be enough?")
+elif answer == "seek help from the Gray family":
+    print("At least you get the 'get help' part right.")
+elif answer == "become a drug kingpin and a professional methampethamine manufacturer":
+    print("You're goddamn right!")
+else:
+    print("You haven't watched the show, have you?")
